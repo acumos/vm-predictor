4202016ef22f67abef7e4f51e29392b435622e68
Michael Tinnemeier (Michael) (mt531r) <mtinnemeier@vision5.research.att.com>
2017-05-18 13:18:35 -0400

    crome.py:  assorted minor cleanups

--- crome.py
+++ crome.py
@@ -36,7 +36,8 @@ def H2O_train_and_predict(train_path, test_path, target_col, feat_cols):
     h2o.remove(test_frame.frame_id)
     h2o.remove(preds.frame_id)
 
-    return predicted
+    return predicted, 'predict'                 # H2O column name
+
     
 
 def get_busy_hour (arr, period):
@@ -62,10 +63,11 @@ def get_busy_avg (arr, period):
   
           
           
-class CromeProcessor:
+class CromeProcessor(object):
     # See http://pandas.pydata.org/pandas-docs/stable/timeseries.html#offset-aliases for resample strings
     def __init__ (self, target_col, date_col='DATETIMEUTC', train_size_days=31, predict_size_days=1, resample_str="15min", png_base_path="." ):
         self.target_col = target_col
+        self.predict_col = ""                   # platform specific
         self.train_interval = pd.Timedelta(days=train_size_days)
         self.predict_interval = pd.Timedelta(days=predict_size_days)
         self.resample_str = resample_str
@@ -82,29 +84,13 @@ class CromeProcessor:
         self.showRaw = True
                
     
-    def process_file (self, filename):
+    def process_CSVfile (self, filename):
         df = pd.read_csv(filename)
         print (">> %s: %s rows" % (filename, len(df)))
         df, features = self.transform_dataframe (df)
         df_result = self.predict_sliding_windows (df, features)
-        output = {}
-        if len(df_result) > 0:
-            if self.showRaw:
-                self.add_view (output, "original", df_result, False)
-            if self.pctile:
-                self.add_view (output, "percentile_%s" % self.pctile, df_result.resample("1D").apply(lambda x: np.percentile(x, self.pctile)), True)
-            if self.STD:
-                self.add_view (output, "std", df_result.resample("1D").apply(lambda x: np.std(x)), False)
-            if self.VAR:
-                self.add_view (output, "variance", df_result.resample("1D").apply(lambda x: np.var(x)), False)
-            if self.busyHours:
-                df_hour = df_result.resample("1H").mean()
-                self.add_view (output, "busy_hour_%sH" % self.busyHours, df_hour.resample("1D").apply(lambda x: get_busy_hour(x, self.busyHours).hour), False)
-                self.add_view (output, "busy_avg_%sH" % self.busyHours, df_hour.resample("1D").apply(lambda x: get_busy_avg(x, self.busyHours)), True)
-        else:
-            print (">> insufficient data")
-        return output
-            
+        return self.build_views (df_result)
+
     
     def transform_dataframe (self, df):       # could be a pipeline
         # 1. convert to datetime index
@@ -134,7 +120,6 @@ class CromeProcessor:
 
     
     def predict_sliding_windows (self, df, feat_cols):        # presumed that the incoming dataframe contains ONLY rows of interest
-        predict_col = 'predict'                 # H2O convention
         train_start = df.index[0]
         result = pd.DataFrame()
         while True:
@@ -154,12 +139,12 @@ class CromeProcessor:
             df_train.to_csv(self.training_file_name, index=False)
             df_test.to_csv(self.testing_file_name, index=False)
             
-            preds = H2O_train_and_predict(self.training_file_name, self.testing_file_name, self.target_col, feat_cols)
+            preds, self.predict_col = H2O_train_and_predict(self.training_file_name, self.testing_file_name, self.target_col, feat_cols)
             
             # append to result dataframe
-            kwargs = {predict_col : preds[predict_col].values}
+            kwargs = {self.predict_col : preds[self.predict_col].values}
             df_test = df_test.assign (**kwargs)
-            result = pd.concat([result, df_test[[predict_col, self.target_col]]])
+            result = pd.concat([result, df_test[[self.predict_col, self.target_col]]])
             
             #remove_files ([training_file_name, testing_file_name])
             train_start += self.predict_interval
@@ -167,10 +152,32 @@ class CromeProcessor:
         return result
 
 
+    def build_views (self, df):
+        output = []
+        if len(df) > 0:
+            if self.showRaw:
+                self.add_view (output, "original", df, False)
+            if self.pctile:
+                self.add_view (output, "percentile_%s" % self.pctile, df.resample("1D").apply(lambda x: np.percentile(x, self.pctile)), True)
+            if self.STD:
+                self.add_view (output, "std", df.resample("1D").apply(lambda x: np.std(x)), False)
+            if self.VAR:
+                self.add_view (output, "variance", df.resample("1D").apply(lambda x: np.var(x)), False)
+            if self.busyHours:
+                df_hour = df.resample("1H").mean()
+                self.add_view (output, "busy_hour_%sH" % self.busyHours, df_hour.resample("1D").apply(lambda x: get_busy_hour(x, self.busyHours).hour), False)
+                self.add_view (output, "busy_avg_%sH" % self.busyHours, df_hour.resample("1D").apply(lambda x: get_busy_avg(x, self.busyHours)), True)
+        else:
+            print (">> insufficient data")
+        return output
+        
+
     def add_view (self, result_obj, view_name, dataframe, calc_error = False):
-        result_obj[view_name] = {"data":dataframe}
+        entry = {"type":view_name, "data":dataframe}
         if calc_error:
-            result_obj[view_name]["error"] = self.calc_AAE (dataframe)
+            entry["error"] = self.calc_AAE (dataframe)        
+        result_obj.append(entry)
+        return result_obj
         
 
     def calc_AAE (self, df):               # input:  two pd.Series
@@ -183,32 +190,32 @@ class CromeProcessor:
         aae = numerator / denominator
         return aae.mean()
 
-        
+            
     def draw_charts (self, charts, filename):       
         for chart in charts:
-            df = charts[chart]["data"]
-            title = self.target_col + ":  " + chart + "\n"
+            df = chart["data"]
+            title = self.target_col + ":  " + chart["type"] + "\n"
             title += filename
-            if "error" in charts[chart]:
-                title += "  (err=%s)" % charts[chart]["error"]
+            if "error" in chart:
+                title += "  (err=%s)" % chart["error"]
             title += "\nunit=" + self.resample_str + ", train=%dd, test=%dd" % (self.train_interval.days, self.predict_interval.days)
-            outfile = self.compose_chart_name (filename, chart)
+            outfile = self.compose_chart_name (filename, chart["type"])
             draw_chart(title, df['predict'], df[self.target_col], df.index, outfile)
 
-        
+
     def draw_compound_chart (self, charts, filename):
         if len(charts) > 0:
             outfile = self.compose_chart_name (filename)
             ch_list = []
             for chart in charts:
-                df = charts[chart]["data"]
-                title = chart
-                if "error" in charts[chart]:
-                    title += " (err=%5.3f)" % charts[chart]["error"]
+                df = chart["data"]
+                title = chart["type"]
+                if "error" in chart:
+                    title += " (err=%5.3f)" % chart["error"]
                 ch_list.append ((title, df['predict'], df[self.target_col], df.index))
-            bigtitle = "%s %s unit=%s train=%dd test=%dd" % (filename, self.target_col, self.resample_str, self.train_interval.days, self.predict_interval.days)
+            bigtitle = "%s %s unit=%s train=%dd test=%dd" % (self.target_col, filename, self.resample_str, self.train_interval.days, self.predict_interval.days)
             draw_multi_charts (ch_list, bigtitle, outfile)
-        
+            
         
     def compose_chart_name(self, entity, chart_type="", subscriber=None):
         output_path = join(self.png_base, self.target_col)
@@ -301,18 +308,23 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "CROME training and testing", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-t', '--target', help='target prediction column', default='cpu_usage')
     parser.add_argument('-d', '--dir', help = 'directory containing CSV files', type=str)
-    parser.add_argument('-c', '--compound', help = 'output single compound chart', action='store_true')
+    parser.add_argument('-c', '--compound', help = 'output compound charts', action='store_true')
+    parser.add_argument('-s', '--separate', help = 'output separate charts', action='store_true')
+    parser.add_argument('-r', '--randomize', help = 'randomize file list', action='store_true')
     parser.add_argument('-p', '--png_dir', help = 'destination directory for PNG files', default='./png')
     parser.add_argument('-n', '--max_files', help = 'process at most N files', type=int, default=1000000)
     parser.add_argument('files', nargs='+', help='list of CSV files to process')
     cfg = parser.parse_args()
-        
+
+    if cfg.randomize:
+        from random import shuffle
+        shuffle (cfg.files)
     cp = CromeProcessor ('cpu_usage', png_base_path=cfg.png_dir)
     for fname in cfg.files[:cfg.max_files]:
-        results = cp.process_file(fname)
+        results = cp.process_CSVfile (fname)
         if cfg.compound:
             cp.draw_compound_chart(results, basename(fname))
-        else:
+        if cfg.separate:
             cp.draw_charts(results, basename(fname))
     
     
