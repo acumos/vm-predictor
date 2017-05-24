f22b6c8269ddc78ac0dd8f54053f9dbdb16cab0e
Michael Tinnemeier (Michael) (mt531r) <mtinnemeier@vision5.research.att.com>
2017-05-24 15:02:23 -0400

    add_FEAT_data:  more flexibility via command line
    args

--- add_FEAT_data.py
+++ add_FEAT_data.py
@@ -38,30 +38,30 @@ def trim_columns (df, columns):
 
     
 
-def main(input_filename, dest_dir, float_cols, trim_cols, VM_list=[]):
+def main(input_filename, dest_dir, float_cols, trim_cols, max_proc, VM_list=[]):
     print ("read file", input_filename)
-
     df = pd.read_csv(input_filename)
 
     print ("remove spaces")
     df = remove_column_spaces(df)
     
     print ("collect VMs")
+    to_trim = list(trim_cols)
     df = trim_columns (df, ['VM_ID'])
-    trim_cols.remove('VM_ID')
+    to_trim.remove('VM_ID')
     
     if not VM_list or len(VM_list) == 0:
-        VM_list = list(set(df['VM_ID']))            # process all VMs
+        VM_list = sorted(list(set(df['VM_ID'])))            # process all VMs
     else:
         df = df[df['VM_ID'].isin(VM_list)]
     
     print ("convert columns to float:", float_cols)
     df = cols_to_float (df, float_cols)
     
-    print ("apply trim:", trim_cols)
-    df = trim_columns (df, trim_cols)
+    print ("apply trim:", to_trim)
+    df = trim_columns (df, to_trim)
 
-    for vm in VM_list:
+    for vm in VM_list[:max_proc]:
         df_vm = df[df['VM_ID']==vm]
         print ("processing VM: %s (%s rows)" % (vm, len(df_vm)))
         output_filename = join(dest_dir, vm + ".csv")
@@ -86,17 +86,16 @@ if __name__ == "__main__":
     trim_cols = ["DATETIMEUTC", "DATEUTC", "GLOBAL_CUSTOMER_ID", "SUBSCRIBER_NAME", "VM_ID"]
     
     parser = argparse.ArgumentParser(description = "CROME data import tool", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('input_file', help='name of FEAT_VM*.csv file to import')
-    parser.add_argument('output_dir', help='destination directory containing VM data')
+    parser.add_argument('input_files', nargs='+', help='name of FEAT_VM*.csv file(s) to import')
+    parser.add_argument('-o', '--output_dir', default='.', help='destination directory containing VM data')
     parser.add_argument('-v', '--vm_list', nargs='+', help='list of VMs to process (default=all)')
+    parser.add_argument('-n', '--max_vm', help = 'process at most N entities per input file', type=int, default=1000000)
     
     cfg = parser.parse_args()
     
-    #main (cfg.input_file, cfg.output_dir, float_cols, trim_cols, VM_list=["0625561c-8305-44be-afea-6b2bd6d3cdb0"])
-    #main (cfg.input_file, cfg.output_dir, float_cols, trim_cols, VM_list=["0c1babd4-9ff2-4891-8da8-908d83c9c758"])
-    main (cfg.input_file, cfg.output_dir, float_cols, trim_cols, VM_list=cfg.vm_list)
-    
- 
+    for feat_file in cfg.input_files:
+        main (feat_file, cfg.output_dir, float_cols, trim_cols, cfg.max_vm, VM_list=cfg.vm_list)
+     
     
 
 
