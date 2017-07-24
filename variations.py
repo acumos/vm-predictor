8a6137fde513617cffa36a61ee9a9c1c5cc5ff85
Michael Tinnemeier (Michael) (mt531r) <mtinnemeier@vision5.research.att.com>
2017-07-24 11:53:14 -0400

    added grid search for net_usage.  Note:  many VMs
    with all-zero data skew results.

--- variations.py
+++ variations.py
@@ -9,19 +9,13 @@ from sklearn.pipeline import Pipeline
 
 from StringColumnEncoder import StringColumnEncoder
 
-
-
-
-#input_file = "FEAT_sampled_small.csv";  print ("USING TEST FILE !!!")
-input_file = "big_sample.csv"
-result_file = "results_var.txt"
-
-
-choices = {'feature_type':  ['STD', 'VM'], 'estimators': [5, 10, 20, 100], 'model': ['RF', 'ET', 'SC_RF', 'SC_ET']}
-
+import numpy as np
 
 
 def run_variation(var_name, cp):
+    input_file = "big_sample.csv"
+    result_file = "results_var.txt"
+
     print ("")
     print ("RUNNING VARIATION:  ", var_name)
     result, VM_list = cp.process_CSVfiles([input_file])
@@ -34,10 +28,13 @@ def run_variation(var_name, cp):
         if len(df) > 0:
             err = cp.calc_AAE (df)
             print ("    score for %s = %s" % (vm, err))
-            errors.append(err)
-            weight = len(df)
-            sum_error += weight * err
-            total_weight += weight
+            if err >= 0.99:
+                import pdb; pdb.set_trace()
+            if not np.isnan(err):
+                errors.append(err)
+                weight = len(df)
+                sum_error += weight * err
+                total_weight += weight
 
     overall_error = sum_error / total_weight
     print ("OVERALL ERROR: ", overall_error)
@@ -47,35 +44,25 @@ def run_variation(var_name, cp):
 
 
     
-ft = ['day', 'weekday', 'hour', 'minute', 'hist-1D8H', 'hist-1D4H', 'hist-1D2H', 'hist-1D1H', 'hist-1D', 'hist-1D15m', 'hist-1D30m', 'hist-1D45m']
-sh = ['day', 'weekday', 'hour', 'minute', 'hist-1H', 'hist-4H', 'hist-1D', 'hist-15m']
+def variations (target):    
+    ft = ['day', 'weekday', 'hour', 'minute', 'hist-1D8H', 'hist-1D4H', 'hist-1D2H', 'hist-1D1H', 'hist-1D', 'hist-1D15m', 'hist-1D30m', 'hist-1D45m']
+    sh = ['day', 'weekday', 'hour', 'minute', 'hist-1H', 'hist-4H', 'hist-1D', 'hist-15m']
+    run_variation (target+" RF_20 std_features", CromeProcessor(target, feats=ft, model=RandomForestRegressor(n_estimators=20)))
+    run_variation (target+" SC_RF_20 std_features", CromeProcessor(target, feats=ft, model=Pipeline([('enc', StringColumnEncoder()), ('sc', StandardScaler()), ('rf', RandomForestRegressor(n_estimators=20))])))
+    run_variation (target+" ET_20 std_features", CromeProcessor(target, feats=ft, model=ExtraTreesRegressor(n_estimators=20)))
+    run_variation (target+" RF_10 std_features", CromeProcessor(target, feats=ft, model=RandomForestRegressor(n_estimators=10)))
+    run_variation (target+" RF_20 vm_features", CromeProcessor(target, feats=ft+['VM_ID'], model=Pipeline([('enc', StringColumnEncoder()), ('rf', RandomForestRegressor(n_estimators=20))])))
+    run_variation (target+" RF_20 sh_features", CromeProcessor(target, feats=sh, model=RandomForestRegressor(n_estimators=20)))
+    run_variation (target+" SC_ET_10 vm_features", CromeProcessor(target, feats=ft+['VM_ID'], model=Pipeline([('enc', StringColumnEncoder()), ('sc', StandardScaler()), ('et', ExtraTreesRegressor(n_estimators=10))])))
+    run_variation (target+" RF_100 std_features", CromeProcessor(target, feats=ft, model=RandomForestRegressor(n_estimators=100)))
+    run_variation (target+" SC_RF_20 vm_features", CromeProcessor(target, feats=ft+['VM_ID'], model=Pipeline([('enc', StringColumnEncoder()), ('sc', StandardScaler()), ('rf', RandomForestRegressor(n_estimators=20))])))
+    run_variation (target+" SC_ET_5 vm_features", CromeProcessor(target, feats=ft+['VM_ID'], model=Pipeline([('enc', StringColumnEncoder()), ('sc', StandardScaler()), ('et', ExtraTreesRegressor(n_estimators=5))])))
 
-'''
-#run_variation ("TESTING #1", CromeProcessor('cpu_usage', feats=ft, predict_size_days=14, model=Pipeline([('enc', StringColumnEncoder()), ('sc', StandardScaler()), ('rf', RandomForestRegressor(n_estimators=20))])))          # TESTING ONLY!!!
-run_variation ("cpu_usage RF_20 std_features", CromeProcessor('cpu_usage', feats=ft, model=RandomForestRegressor(n_estimators=20)))
+    
 
-#DONE run_variation ("cpu_usage SC_RF_20 std_features", CromeProcessor('cpu_usage', feats=ft, model=Pipeline([('enc', StringColumnEncoder()), ('sc', StandardScaler()), ('rf', RandomForestRegressor(n_estimators=20))])))
-#run_variation ("cpu_usage ET_20 std_features", CromeProcessor('cpu_usage', feats=ft, model=Pipeline([('enc', StringColumnEncoder()), ('et', ExtraTreesRegressor(n_estimators=20))])))
-run_variation ("cpu_usage ET_20 std_features", CromeProcessor('cpu_usage', feats=ft, model=ExtraTreesRegressor(n_estimators=20)))
-
-#run_variation ("cpu_usage RF_10 std_features", CromeProcessor('cpu_usage', feats=ft, model=Pipeline([('enc', StringColumnEncoder()), ('rf', RandomForestRegressor(n_estimators=10))])))
-run_variation ("cpu_usage RF_10 std_features", CromeProcessor('cpu_usage', feats=ft, model=RandomForestRegressor(n_estimators=10)))
-
-run_variation ("cpu_usage RF_20 vm_features", CromeProcessor('cpu_usage', feats=ft+['VM_ID'], model=Pipeline([('enc', StringColumnEncoder()), ('rf', RandomForestRegressor(n_estimators=20))])))
-
-#run_variation ("cpu_usage RF_20 sh_features", CromeProcessor('cpu_usage', feats=sh, model=Pipeline([('enc', StringColumnEncoder()), ('rf', RandomForestRegressor(n_estimators=20))])))
-run_variation ("cpu_usage RF_20 sh_features", CromeProcessor('cpu_usage', feats=sh, model=RandomForestRegressor(n_estimators=20)))
-
-run_variation ("cpu_usage SC_ET_10 vm_features", CromeProcessor('cpu_usage', feats=ft+['VM_ID'], model=Pipeline([('enc', StringColumnEncoder()), ('sc', StandardScaler()), ('et', ExtraTreesRegressor(n_estimators=10))])))
-
-#run_variation ("cpu_usage RF_100 std_features", CromeProcessor('cpu_usage', feats=ft, model=Pipeline([('enc', StringColumnEncoder()), ('rf', RandomForestRegressor(n_estimators=100))])))
-run_variation ("cpu_usage RF_100 std_features", CromeProcessor('cpu_usage', feats=ft, model=RandomForestRegressor(n_estimators=100)))
-'''
-
-run_variation ("cpu_usage SC_RF_20 vm_features", CromeProcessor('cpu_usage', feats=ft+['VM_ID'], model=Pipeline([('enc', StringColumnEncoder()), ('sc', StandardScaler()), ('rf', RandomForestRegressor(n_estimators=20))])))
-
-run_variation ("cpu_usage SC_ET_5 vm_features", CromeProcessor('cpu_usage', feats=ft+['VM_ID'], model=Pipeline([('enc', StringColumnEncoder()), ('sc', StandardScaler()), ('et', ExtraTreesRegressor(n_estimators=5))])))
-
-
-
+if __name__ == "__main__":
+    import sys
+    target = sys.argv[1]
+    variations(target)
+    
 
