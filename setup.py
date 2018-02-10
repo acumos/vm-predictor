2935221013a0c0adc0a9d91318801707432e6589
Eric Z <ezavesky@research.att.com>
2018-02-10 06:09:28 -0600

    - setup fix for license scan
    - add tox, minor additions to documentation
    - add setup depednency, but it should not be installed as a packge!
    - include some warning and other files as data for one-place install
    
    Change-Id: Ic4531347afb7d34dca4b448a64fa44be29d0bcfe
    Signed-off-by: Eric Z <ezavesky@research.att.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
-from setuptools import setup   # , find_packages
+from setuptools import setup, find_packages
 from setuptools.command.install import install
-
-# warning (if run in verbose mode) about installing this object
-class new_install(install):
-    def run(self):
-        with open(os.path.join(setup_dir,"INSTALL.txt")) as f:
-            print(f.read())
-        install.run(self)
-
 
 # extract __version__ from version file. importing will lead to install failures
 setup_dir = os.path.dirname(__file__)
@@ -17,22 +9,31 @@ with open(os.path.join(setup_dir, 'vm_predictor', '_version.py')) as file:
     globals_dict = dict()
     exec(file.read(), globals_dict)
     __version__ = globals_dict['__version__']
+    __model_name__ = globals_dict['__model_name__']
+
+
+# warning (if run in verbose mode) about installing this object
+class new_install(install):
+    def run(self):
+        with open(os.path.join(setup_dir, __model_name__, "INSTALL.txt")) as f:
+            print(f.read())
+        install.run(self)
 
 
 # read requirements list from supplementary file in this repo
-requirement_list = [ line for line in open(os.path.join(setup_dir,'requirements.txt')) if line and line[0] != '#' ]
+requirement_list = [line for line in open(os.path.join(setup_dir, __model_name__, 'requirements.txt')) if line and line[0] != '#']
 
 
 setup(
-    name='vm_predictor',
+    name=__model_name__,
     version=__version__,
-    packages=[],
+    packages=find_packages(),  # NOTE - THIS SHOULD NOT BE AN INSTALLED PACKAGE!
     author="Michael Tinnemeier",
     author_email="ezavesky@research.att.com",
     description=("VM resource predictor based on historical data and context"),
     long_description=("VM resource predictor based on historical data and context"),
     license="Apache",
-    # package_data={globals_dict['MODEL_NAME']: ['data/*']},
+    package_data={__model_name__: ['requirements.txt', 'INSTALL.txt']},
     # setup_requires=['pytest-runner'],
     entry_points="""
     [console_scripts]
